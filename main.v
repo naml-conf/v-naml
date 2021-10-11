beafb29403296984cae45effd261c3f49d20adf1
MrsHerobrine (Naomi) <mrsherobrinenaomi@gmail.com>
2021-10-11 21:52:48 +0200

    actually make stuff tokenize correctly

--- main.v
+++ main.v
@@ -1,3 +1,5 @@
+//module naml
+
 import os 
 
 fn main() {
@@ -10,13 +12,56 @@ fn main() {
 	}
 
 	for line in lines {
-
 		//TODO make it find the correct type
-		token_list << TokenString{Tokens.text, line}
+		match true {
+			line.contains('{') {
+				a, b := chop_up_block_open(line)
+				c := a.trim(' ')
+				token_list << Token{ Tokens.key, c }
+				token_list << Token{ Tokens.block_open, b}
+			}
+			
+			line.contains('}') {
+				a := chop_up_block_close(line)
+				c := a.trim(' ')
+				token_list << Token{ Tokens.block_close, c }
+			}
+
+			line.contains('=') {
+				a, b := chop_up(line)
+				c := a.trim(' ')
+				token_list << Token{ Tokens.key, c}
+
+				match true {
+					b.contains('.') {
+						token_list << Token{ Tokens.double, b }
+					}
+
+					b.contains('y') {
+						token_list << Token{ Tokens.bool_true, b }
+					}
+
+					b.contains('n') {
+						token_list << Token{ Tokens.bool_false, b }
+					}
+
+					b.contains('"') {
+						token_list << Token{ Tokens.text, b }
+					}
+
+					b.contains_any('1234567890') {
+						token_list << Token{ Tokens.integer, b}
+					}
+
+					else { panic('Wrong value!') }
+				}
+			}
+			else { panic('Parse error!') }
+		}
 	}
 
 	for token in token_list {
-		println(token.value)
+		println(token.str())
 	}
 }
 
@@ -32,32 +77,27 @@ enum Tokens {
 	equals		// an equals sign
 }
 
-interface Token {
-	token_type	Tokens
-	value		any
-}
-
-struct TokenString {
+struct Token {
 	token_type	Tokens
 	value 		string
 }
 
-struct TokenDouble {
-	token_type	Tokens
-	value 		f64
+
+// this should only be used when we're sure there's a value being defined
+fn chop_up(s string) (string, string) {
+	return s.all_before(' ='), s.all_after('= ')
 }
 
-struct TokenInt {
-	token_type	Tokens
-	value 		int
+// chops up a block opening
+fn chop_up_block_open(s string) (string, string) {
+	return s.all_before(' {'), s.all_after_last(' ')
 }
 
-struct TokenBool {
-	token_type	Tokens
-	value 		bool
+// chops up a block closing
+fn chop_up_block_close(s string) (string) {
+	return s
 }
 
-struct TokenBlock {
-	token_type	Tokens
-	value		[]Token
+fn (t Token) str() string {
+	return 'Token( type: $t.token_type, value: $t.value )'
 }
