87f94d4516c1545ebdf7a9c7848c4aeeb802f076
mrsherobrine <mrsherobrinenaomi@gmail.com>
2021-10-13 12:32:40 +0200

    i gained IQ points writing this

--- main.v
+++ main.v
@@ -12,6 +12,78 @@ pub fn main() {
 		return
 	}
 
+	//for i in 0 .. 5 {
+		tokenize(lines, mut token_list)
+	//	println(i)
+	//}
+
+	blub := parse(mut token_list)
+
+	for b in blub {
+		println(b.content)
+	}
+	
+	//println('Mapped in ${f64(sw.elapsed().nanoseconds())/1000000.0}ms')
+}
+
+enum Tokens {
+    text		// ""
+	block_open	// {
+	block_close	// }
+	integer		// i32
+	double		// f64
+	bool_true	// y
+	bool_false	// n
+	key			// name of a value
+}
+
+enum DataType{
+	string
+	int
+	f64
+	bool
+	block
+}
+
+struct Block {
+	block []NamlNode
+}
+
+struct Token {
+	token_type	Tokens
+	value 		string
+}
+
+type NamlData = string | bool | int | f64 | Block
+
+struct NamlNode{
+	//name		string
+	content		NamlData
+	value		DataType
+}
+
+
+// this should only be used when we're sure there's a value being defined
+fn chop_up(s string) (string, string) {
+	return s.all_before(' ='), s.all_after('= ')
+}
+
+// chops up a block opening
+fn chop_up_block_open(s string) (string, string) {
+	return s.all_before(' {'), s.all_after_last(' ')
+}
+
+// chops up a block closing
+fn chop_up_block_close(s string) (string) {
+	return s
+}
+
+fn (t Token) str() string {
+	return 'Token( type: $t.token_type, value: $t.value )'
+}
+
+[inline]
+fn tokenize(lines []string, mut token_list []Token) {
 	sw := time.new_stopwatch()
 
 	//to keep track of which line we're on in case of error
@@ -19,6 +91,7 @@ pub fn main() {
 
 	for line in lines {
 		index++
+		trimmed_line := line.trim(' ')
 		match true {
 			line.contains('{') {
 				a, b := chop_up_block_open(line)
@@ -37,7 +110,6 @@ pub fn main() {
 				a, b := chop_up(line)
 				c := a.trim(' ')
 				token_list << Token{ Tokens.key, c }
-				trimmed_line := line.trim(' ')
 				match true {
 					b.contains('.') {
 						if b.count('.') == 1 {
@@ -82,58 +154,57 @@ pub fn main() {
 			}
 
 			else { 
-				trimmed_line := line.trim(' ')
 				panic('Missing block open/close or value declaration\nAt line $index: $trimmed_line') 
 			}
 		}
 	}
 
 	println('Tokenized in ${f64(sw.elapsed().nanoseconds())/1000000.0}ms')
-
-	mut token_map := map[string]TokenValue{}
-	token_map['h'] = TokenValue(TokenMap{})
-	nested_map := token_map['h'] or { panic('bad') }
-	nested_map['h'] = 'h'
-
-	println('Mapped in ${f64(sw.elapsed().nanoseconds())/1000000.0}ms')
 }
 
-enum Tokens {
-    text		// ""
-	block_open	// {
-	block_close	// }
-	integer		// i32
-	double		// f64
-	bool_true	// y
-	bool_false	// n
-	key			// name of a value
-	equals		// an equals sign
-}
+[inline]
+fn parse(mut token_list []Token) []&NamlNode {
 
-type TokenValue = bool | string | int | f64 | TokenMap
-type TokenMap = map[string]TokenValue
+	mut naml_list :=  []&NamlNode{}
 
-struct Token {
-	token_type	Tokens
-	value 		string
-}
+	for i, token in token_list {
+		typ := token.token_type
+		match typ {
 
+			.integer {
+				naml_list << &NamlNode{ token.value, DataType.int }
+			}
 
-// this should only be used when we're sure there's a value being defined
-fn chop_up(s string) (string, string) {
-	return s.all_before(' ='), s.all_after('= ')
-}
+			.text {
+				naml_list << &NamlNode{ token.value, DataType.string }
+			}
+			
+			.block_open {
+				naml_list << &NamlNode{ Block{}, DataType.block }
+			}
 
-// chops up a block opening
-fn chop_up_block_open(s string) (string, string) {
-	return s.all_before(' {'), s.all_after_last(' ')
-}
+			.double {
+				naml_list << &NamlNode{ token.value, DataType.f64 }
+			}
 
-// chops up a block closing
-fn chop_up_block_close(s string) (string) {
-	return s
-}
+			.bool_false {
+				naml_list << &NamlNode{ false, DataType.bool }
+			}
 
-fn (t Token) str() string {
-	return 'Token( type: $t.token_type, value: $t.value )'
+			.bool_true {
+				naml_list << &NamlNode{ true, DataType.bool }
+			}
+
+			.key {
+				naml_list << &NamlNode { token.value}
+			}
+
+			else {
+				println('h')
+			}
+		}
+	}
+
+	return naml_list
+
 }
